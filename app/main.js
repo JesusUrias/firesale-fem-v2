d76c3e603ddd8b2f76355b60de1ae3fd57469232
JesusUrias <55812647+JesusUrias@users.noreply.github.com>
2022-09-12 19:02:45 -0700

    Carpeta del curso de Electron

--- app/main.js
+++ app/main.js
@@ -1,1 +1,15 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow( { show: false}); // Create the browser window.
+
+    mainWindow.loadFile(`${__dirname}/index.html`); // Load a local HTML file
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show(); // Show window when page is ready
+    });
+});
+
+console.log('starting up');
